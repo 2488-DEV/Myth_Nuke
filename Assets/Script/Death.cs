0148e9b3247d5ce1f0c3c5929102724b8dcf8066
LionFire2488 <wongsathornsirisombat@gmail.com>
2026-09-24 14:45:35 +0700

    START

---
+++ Assets/Script/Death.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject deadCanvas;
+    public GameObject menuCanvas;
+    public AudioSource audioSource;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Object") || collision.gameObject.CompareTag("Ground"))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Dead");
+        audioSource.Play();
+        player.SetActive(false);
+        deadCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Script/Load_Play_Scene.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Play_Scene : MonoBehaviour
+{
+    public void Load_In_Game_Scene()
+    {
+        SceneManager.LoadScene("In_Game");
+        Trigger_Score.score = 0;
+    }
+
+    public void Load_Main_Menu_Scene()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    public void Load_Learn_Nuclear_Scene()
+    {
+        SceneManager.LoadScene("LearnNuclear");
+    }
+
+    public void Load_Quiz_1_Scene()
+    {
+        SceneManager.LoadScene("Quiz_1");
+        Quiz_Score.score = 0;
+    }
+
+    public void Load_Quiz_2_Scene()
+    {
+        SceneManager.LoadScene("Quiz_2");
+    }
+
+    public void Load_Quiz_3_Scene()
+    {
+        SceneManager.LoadScene("Quiz_3");
+    }
+    
+    public void Load_Quiz_4_Scene()
+    {
+        SceneManager.LoadScene("Quiz_4");
+    }
+    
+    public void Load_Quiz_5_Scene()
+    {
+        SceneManager.LoadScene("Quiz_5");
+    }
+
+    public void Load_Quiz_6_Scene()
+    {
+        SceneManager.LoadScene("Quiz_6");
+    }
+
+    public void Load_Quiz_7_Scene()
+    {
+        SceneManager.LoadScene("Quiz_7");
+    }
+
+    public void Load_Quiz_8_Scene()
+    {
+        SceneManager.LoadScene("Quiz_8");
+    }
+
+    public void Load_Quiz_9_Scene()
+    {
+        SceneManager.LoadScene("Quiz_9");
+    }
+
+    public void Load_Quiz_10_Scene()
+    {
+        SceneManager.LoadScene("Quiz_10");
+    }
+
+    public void Load_Final_Quiz_Scene()
+    {
+        SceneManager.LoadScene("Final_Quiz");
+    }
+    
+    public void Load_Myth_Vs_Facts_Scene()
+    {
+        SceneManager.LoadScene("MythVsFacts");
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }   
+}
